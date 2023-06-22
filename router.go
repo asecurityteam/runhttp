a2e32c5cc7ca5bb03791a18ffa871de718908524
Brendan Shaklovitz <nyanshak@users.noreply.github.com>
2023-06-22 15:20:17 -0500

    Update deps

--- router.go
+++ router.go
@@ -1,18 +1,17 @@
 package runhttp
 
 import (
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 )
 
 // RouterConfig is used as a simple default for NewDefaultRouter
-type RouterConfig struct {
-}
+type RouterConfig struct{}
 
 // NewDefaultRouter generates a mux.
 // This version returns a mux from the chi project
 // as a convenience for cases where custom middleware or additional
 // routes need to be configured.
-func NewDefaultRouter(conf *RouterConfig) *chi.Mux {
+func NewDefaultRouter(_ *RouterConfig) *chi.Mux {
 	router := chi.NewMux()
 	healthCheckHandler := &HealthCheckHandler{}
 
