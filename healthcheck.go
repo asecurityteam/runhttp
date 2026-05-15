b5a0178cd45b32f8373ff4a96c2d78cb2bd81560
Grygoriy Ensary <gensary@atlassian.com>
2026-05-15 11:42:23 -0500

    https://sonarcloud.io/project/issues?issueStatuses=OPEN&id=asecurityteam_runhttp&open=AZst052-358L6je7ETt9 warning re: unused func argument

--- healthcheck.go
+++ healthcheck.go
@@ -7,7 +7,7 @@ type HealthCheckHandler struct {
 }
 
 // Handle responds with a 200 by default
-func (h *HealthCheckHandler) Handle(w http.ResponseWriter, r *http.Request) {
+func (h *HealthCheckHandler) Handle(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("Success"))
 }
